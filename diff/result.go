3b2c7d5662796870b67f56613b7a518c6e94a997
Reuven <rh@tufin.com>
2021-02-20 20:30:36 +0200

    doc

--- diff/result.go
+++ diff/result.go
@@ -2,13 +2,16 @@ package diff
 
 import "github.com/getkin/kin-openapi/openapi3"
 
-// Result contains a diff and a summary
 type Result struct {
 	Diff    *Diff    `json:"diff,omitempty"`
 	Summary *Summary `json:"summary,omitempty"`
 }
 
-// Run returns the diff between two OAS specs including a summary
+/*
+Run calculates the diff between two OAS specs including a summary.
+Prefix is an optional path prefix that exists in s1 endpoints but not in s2.
+If filter isn't empty, the diff will only include endpoints that match this regex.
+*/
 func Run(s1 *openapi3.Swagger, s2 *openapi3.Swagger, prefix string, filter string) Result {
 	diff := getDiff(s1, s2, prefix)
 	diff.filterByRegex(filter)
