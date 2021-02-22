d95b37083592065d8cbc814137c35928337da58e
Reuven <rh@tufin.com>
2021-02-22 17:40:36 +0200

    additional aspects of method

--- diff/method_diff.go
+++ diff/method_diff.go
@@ -6,8 +6,20 @@ import (
 
 // MethodDiff is the diff between two OAS operations (methods)
 type MethodDiff struct {
-	ParamDiff    *ParametersDiff `json:"parameters,omitempty"`
-	ResponseDiff *ResponsesDiff  `json:"responses,omitempty"`
+
+	// ExtensionProps
+	// Tags
+	SummaryDiff     *ValueDiff      `json:"summary,omitempty"`     // diff of 'summary' property
+	DescriptionDiff *ValueDiff      `json:"description,omitempty"` // diff of 'description' property
+	OperationIDDiff *ValueDiff      `json:"operationID,omitempty"` // diff of 'operationID' property
+	ParamDiff       *ParametersDiff `json:"parameters,omitempty"`  // diff of 'parameters' property
+	// RequestBody
+	ResponseDiff *ResponsesDiff `json:"responses,omitempty"` // diff of 'responses' property
+	// Callbacks
+	DeprecatedDiff *ValueDiff `json:"deprecated,omitempty"` // diff of 'deprecated' property
+	// Security
+	// Servers
+	// ExternalDocs
 }
 
 func newMethodDiff() *MethodDiff {
@@ -23,6 +35,10 @@ func getMethodDiff(pathItem1, pathItem2 *openapi3.Operation) *MethodDiff {
 
 	result := newMethodDiff()
 
+	result.SummaryDiff = getValueDiff(pathItem1.Summary, pathItem2.Summary)
+	result.DescriptionDiff = getValueDiff(pathItem1.Description, pathItem2.Description)
+	result.OperationIDDiff = getValueDiff(pathItem1.OperationID, pathItem2.OperationID)
+
 	if diff := getParametersDiff(pathItem1.Parameters, pathItem2.Parameters); !diff.empty() {
 		result.ParamDiff = diff
 	}
@@ -30,6 +46,7 @@ func getMethodDiff(pathItem1, pathItem2 *openapi3.Operation) *MethodDiff {
 	if diff := getResponsesDiff(pathItem1.Responses, pathItem2.Responses); !diff.empty() {
 		result.ResponseDiff = diff
 	}
+	result.DeprecatedDiff = getValueDiff(pathItem1.Deprecated, pathItem2.Deprecated)
 
 	return result
 }
