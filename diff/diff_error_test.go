14dbdbffd60f7c41daccd021399d1aa13f70e7cb
Reuven <rh@tufin.com>
2021-03-15 10:11:48 +0200

    error tests

--- diff/diff_error_test.go
+++ diff/diff_error_test.go
@@ -8,58 +8,40 @@ import (
 	"github.com/tufin/oasdiff/diff"
 )
 
-func TestDiff_NilPointers(t *testing.T) {
+func TestDiff_SchemaNil(t *testing.T) {
 	loader := openapi3.NewSwaggerLoader()
 	s1, err := loader.LoadSwaggerFromFile("../data/home-iot-api-1.yaml")
 	require.NoError(t, err)
-	s2, err := loader.LoadSwaggerFromFile("../data/home-iot-api-2.yaml")
-	require.NoError(t, err)
 
 	s1.Paths["/devices"].Post.RequestBody.Value.Content["application/json"].Schema.Value = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.Error(t, err)
-
-	s1.Paths["/devices"].Post.RequestBody.Value.Content["application/json"].Schema = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.NoError(t, err)
-
-	s1.Paths["/devices"].Post.RequestBody.Value.Content["application/json"] = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.Error(t, err)
-
-	s1.Paths["/devices"].Post.RequestBody.Value.Content = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.NoError(t, err)
-
-	s1.Paths["/devices"].Post.RequestBody = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.NoError(t, err)
-
-	s1.Paths["/devices"].Post = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.NoError(t, err)
-
-	s1.Paths["/devices"] = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.Error(t, err)
-
-	s1.Paths = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
-	require.NoError(t, err)
-
-	s1 = nil
-	_, err = diff.Get(diff.NewConfig(), s1, s2)
+	_, err = diff.Get(diff.NewConfig(), s1, s1)
 	require.Error(t, err)
 }
 
-func TestSchemaDiff_MediaInvalidMultiEntries1(t *testing.T) {
+func TestDiff_ContentNil(t *testing.T) {
+	loader := openapi3.NewSwaggerLoader()
+	s1, err := loader.LoadSwaggerFromFile("../data/home-iot-api-1.yaml")
+	require.NoError(t, err)
 
-	s5 := l(t, 5)
-	s5.Paths[securityScorePath].Get.Parameters.GetByInAndName(openapi3.ParameterInCookie, "test").Content["second/invalid"] = openapi3.NewMediaType()
+	s1.Paths["/devices"].Post.RequestBody.Value.Content["application/json"] = nil
+	_, err = diff.Get(diff.NewConfig(), s1, s1)
+	require.Error(t, err)
+}
 
-	s1 := l(t, 1)
+func TestDiff_PathItemNil(t *testing.T) {
+	loader := openapi3.NewSwaggerLoader()
+	s1, err := loader.LoadSwaggerFromFile("../data/home-iot-api-1.yaml")
+	require.NoError(t, err)
 
-	_, err := diff.Get(diff.NewConfig(), s1, s5)
+	s1.Paths["/devices"] = nil
+	_, err = diff.Get(diff.NewConfig(), s1, s1)
+	require.Error(t, err)
+}
+
+func TestDiff_SpecNil(t *testing.T) {
+	loader := openapi3.NewSwaggerLoader()
+	s1, err := loader.LoadSwaggerFromFile("../data/home-iot-api-1.yaml")
+	_, err = diff.Get(diff.NewConfig(), nil, s1)
 	require.Error(t, err)
 }
 
