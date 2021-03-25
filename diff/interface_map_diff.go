c743f86f162139faeb08a7910cf86fecd8314f22
Reuven <rh@tufin.com>
2021-03-25 12:37:20 +0200

    fix go fmt

--- diff/interface_map_diff.go
+++ diff/interface_map_diff.go
@@ -1,6 +1,6 @@
 package diff
 
-// InterfaceMap is a map of string to interface 
+// InterfaceMap is a map of string to interface
 type InterfaceMap map[string]interface{}
 
 // InterfaceMapDiff describes the changes between a pair of InterfaceMap
