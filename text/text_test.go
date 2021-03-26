eb3a7c6712e06a77014ae8ed28024d7d862e5c3f
Reuven <rh@tufin.com>
2021-03-26 23:02:36 +0300

    simplify tests

--- text/text_test.go
+++ text/text_test.go
@@ -28,37 +28,26 @@ func TestDiff1(t *testing.T) {
 	var buf bytes.Buffer
 	text.Print(d(t, &diff.Config{}, 3, 5), &buf)
 
-	out := buf.String()
-
-	require.Contains(t, out,
-		"GET /api/{domain}/{project}/install-command")
+	require.Contains(t, buf.String(), "GET /api/{domain}/{project}/install-command")
 }
 
 func TestDiff2(t *testing.T) {
 	var buf bytes.Buffer
 	text.Print(d(t, &diff.Config{}, 5, 3), &buf)
 
-	out := buf.String()
-
-	require.Contains(t, out,
-		"GET /api/{domain}/{project}/install-command")
+	require.Contains(t, buf.String(), "GET /api/{domain}/{project}/install-command")
 }
 
 func TestDiff3(t *testing.T) {
 	var buf bytes.Buffer
 	text.Print(d(t, &diff.Config{}, 1, 3), &buf)
 
-	out := buf.String()
-
-	require.Contains(t, out,
-		"GET /api/{domain}/{project}/install-command")
+	require.Contains(t, buf.String(), "GET /api/{domain}/{project}/install-command")
 }
 
 func TestNoDiff(t *testing.T) {
 	var buf bytes.Buffer
 	text.Print(d(t, &diff.Config{}, 3, 3), &buf)
 
-	out := buf.String()
-
-	require.Equal(t, out, "No changes\n")
+	require.Equal(t, buf.String(), "No changes\n")
 }
