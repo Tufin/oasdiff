7ceafc983d264db3bc16cd7af6e10c7723da092b
Reuven <rh@tufin.com>
2021-03-26 13:11:31 +0300

    text param diff

--- text/text.go
+++ text/text.go
@@ -21,22 +21,62 @@ func Print(d *diff.Diff, writer io.Writer) {
 	}
 
 	fmt.Fprintln(writer, "### What's New")
-	fmt.Fprintln(writer, "---")
+	fmt.Fprintln(writer, "--------------")
 	for _, added := range d.EndpointsDiff.Added {
 		fmt.Fprintln(writer, added.Method, added.Path)
 	}
 	fmt.Fprintln(writer, "")
 
 	fmt.Fprintln(writer, "### What's Deprecated")
-	fmt.Fprintln(writer, "---")
+	fmt.Fprintln(writer, "---------------------")
 	for _, deleted := range d.EndpointsDiff.Deleted {
 		fmt.Fprintln(writer, deleted.Method, deleted.Path)
 	}
 	fmt.Fprintln(writer, "")
 
 	fmt.Fprintln(writer, "### What's Changed")
-	fmt.Fprintln(writer, "---")
-	for endpoint := range d.EndpointsDiff.Modified {
+	fmt.Fprintln(writer, "------------------")
+	for endpoint, methodDiff := range d.EndpointsDiff.Modified {
 		fmt.Fprintln(writer, endpoint.Method, endpoint.Path)
+		printMethod(methodDiff, writer)
 	}
+}
+
+func printMethod(d *diff.MethodDiff, writer io.Writer) {
+	if d.Empty() {
+		return
+	}
+
+	if !d.DescriptionDiff.Empty() {
+		fmt.Fprintln(writer, "Description changed from: ", d.DescriptionDiff.From)
+		fmt.Fprintln(writer, "To: ", d.DescriptionDiff.From)
+	}
+
+	printParams(d.ParametersDiff, writer)
+}
+
+func printParams(d *diff.ParametersDiff, writer io.Writer) {
+	if d.Empty() {
+		return
+	}
+
+	for location, params := range d.Added {
+		for _, param := range params {
+			fmt.Fprintln(writer, "* New", location, "param:", param)
+		}
+	}
+
+	for location, params := range d.Deleted {
+		for _, param := range params {
+			fmt.Fprintln(writer, "* Deprecated", location, "param:", param)
+		}
+	}
+
+	for location, paramDiffs := range d.Modified {
+		for param := range paramDiffs {
+			fmt.Fprintln(writer, "* Modified", location, "param:", param)
+		}
+	}
+
+	fmt.Fprintln(writer, "")
 }
