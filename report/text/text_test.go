5359cee1fad93c61d1130dbeb4eefa365f21346e
Reuven <rh@tufin.com>
2021-03-29 20:50:24 +0300

    coverage

--- report/text/text_test.go
+++ report/text/text_test.go
@@ -24,6 +24,37 @@ func d(t *testing.T, config *diff.Config, v1, v2 int) *diff.Diff {
 	return d
 }
 
+func Test_NoChanges(t *testing.T) {
+	var buf bytes.Buffer
+	report := text.Report{
+		Writer: &buf,
+	}
+	report.Output(d(t, &diff.Config{}, 3, 3))
+	require.Equal(t, buf.String(), "No changes\n")
+}
+
+func Test_NoEndpointChanges(t *testing.T) {
+	s1 := openapi3.Swagger{
+		Info: &openapi3.Info{},
+	}
+	s2 := openapi3.Swagger{
+		Info: &openapi3.Info{
+			Title: "reuven",
+		},
+	}
+
+	var buf bytes.Buffer
+	report := text.Report{
+		Writer: &buf,
+	}
+
+	d, err := diff.Get(diff.NewConfig(), &s1, &s2)
+	require.NoError(t, err)
+
+	report.Output(d)
+	require.Equal(t, buf.String(), "No endpoint changes\n")
+}
+
 func TestDiff1(t *testing.T) {
 	var buf bytes.Buffer
 	report := text.Report{
@@ -52,16 +83,6 @@ func TestDiff3(t *testing.T) {
 	report.Output(d(t, &diff.Config{}, 1, 3))
 
 	require.Contains(t, buf.String(), "GET /api/{domain}/{project}/install-command")
-}
-
-func TestNoDiff(t *testing.T) {
-	var buf bytes.Buffer
-	report := text.Report{
-		Writer: &buf,
-	}
-	report.Output(d(t, &diff.Config{}, 3, 3))
-
-	require.Equal(t, buf.String(), "No changes\n")
 }
 
 func TestDiff4(t *testing.T) {
