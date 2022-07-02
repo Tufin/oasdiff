9eada377e45b4c0095f19aed8d53d89730af0796
Reuven <rh@tufin.com>
2022-07-02 13:28:40 +0300

    reorg breaking tests

---
+++ diff/not_breaking_test.go
@@ -0,0 +1,127 @@
+package diff_test
+
+import (
+	"testing"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/diff"
+)
+
+func TestBreaking_Same(t *testing.T) {
+	// no change -> not breaking
+	require.Empty(t, d(t, &diff.Config{BreakingOnly: true}, 1, 1))
+}
+
+func TestBreaking_DeletedTag(t *testing.T) {
+	// deleting a tag isn't breaking
+	require.Empty(t, d(t, &diff.Config{
+		BreakingOnly: true,
+	}, 1, 5).PathsDiff.Modified[securityScorePath].OperationsDiff.Modified["GET"].TagsDiff)
+}
+
+func TestBreaking_AddedEnum(t *testing.T) {
+	// adding an enum value isn't breaking
+	require.Empty(t,
+		d(t, &diff.Config{
+			BreakingOnly: true,
+		}, 1, 3).PathsDiff.Modified[installCommandPath].OperationsDiff.Modified["GET"].ParametersDiff.Modified[openapi3.ParameterInPath])
+}
+
+func TestBreaking_ModifiedExtension(t *testing.T) {
+	// changing extensions isn't breaking
+	config := diff.Config{
+		BreakingOnly:      true,
+		IncludeExtensions: diff.StringSet{"x-extension-test2": struct{}{}},
+	}
+
+	require.Empty(t, d(t, &config, 1, 3).ExtensionsDiff)
+}
+
+func TestBreaking_Components(t *testing.T) {
+	// chaning comments isn't breaking
+	require.Empty(t, d(t, &diff.Config{BreakingOnly: true},
+		1, 3).ComponentsDiff)
+}
+
+func TestBreaking_NewOptionalHeaderParam(t *testing.T) {
+	s1 := l(t, 1)
+	s2 := l(t, 1)
+
+	deleteParam(s1.Paths[installCommandPath].Get, openapi3.ParameterInHeader, "network-policies")
+	s2.Paths[installCommandPath].Get.Parameters.GetByInAndName(openapi3.ParameterInHeader, "network-policies").Required = false
+
+	d, err := diff.Get(&diff.Config{
+		BreakingOnly: true,
+	}, s1, s2)
+	require.NoError(t, err)
+
+	// new optional header param isn't breaking
+	require.Empty(t, d)
+}
+
+func TestBreaking_HeaderParamRequiredDisabled(t *testing.T) {
+	s1 := l(t, 1)
+	s2 := l(t, 1)
+
+	s1.Paths[installCommandPath].Get.Parameters.GetByInAndName(openapi3.ParameterInHeader, "network-policies").Required = true
+	s2.Paths[installCommandPath].Get.Parameters.GetByInAndName(openapi3.ParameterInHeader, "network-policies").Required = false
+
+	d, err := diff.Get(&diff.Config{
+		BreakingOnly: true,
+	}, s1, s2)
+	require.NoError(t, err)
+
+	// changing an existing header param to optional doesn't break client
+	require.Empty(t, d)
+}
+
+func TestBreaking_NewRequiredResponseHeader(t *testing.T) {
+	s1 := l(t, 1)
+	s2 := l(t, 1)
+
+	deleteResponseHeader(s1.Paths[installCommandPath].Get.Responses["default"].Value, "X-RateLimit-Limit")
+	s2.Paths[installCommandPath].Get.Responses["default"].Value.Headers["X-RateLimit-Limit"].Value.Required = true
+
+	d, err := diff.Get(&diff.Config{
+		BreakingOnly: true,
+	}, s1, s2)
+	require.NoError(t, err)
+
+	// new required response header param isn't breaking
+	require.Empty(t, d)
+}
+
+func TestBreaking_OperationID(t *testing.T) {
+	// chaning operation ID isn't breaking
+	require.Empty(t,
+		d(t, &diff.Config{
+			BreakingOnly: true,
+		}, 3, 1).PathsDiff.Modified[securityScorePath].OperationsDiff.Modified["GET"].OperationIDDiff)
+}
+
+func TestBreaking_LinkOperationID(t *testing.T) {
+	// chaning a link to operation ID isn't breaking
+	require.Empty(t,
+		d(t, &diff.Config{
+			BreakingOnly: true,
+		}, 3, 1).PathsDiff.Modified["/subscribe"].OperationsDiff.Modified["POST"].CallbacksDiff.Modified["myEvent"].Modified["hi"].OperationsDiff.Modified["POST"].ResponsesDiff.Modified["200"].LinksDiff.Modified)
+}
+
+func TestBreaking_ResponseAddMediaType(t *testing.T) {
+	loader := openapi3.NewLoader()
+
+	s1, err := loader.LoadFromFile("../data/response-media-type-revision.yaml")
+	require.NoError(t, err)
+
+	s2, err := loader.LoadFromFile("../data/response-media-type-base.yaml")
+	require.NoError(t, err)
+
+	d, err := diff.Get(&diff.Config{
+		BreakingOnly: true,
+	}, s1, s2)
+	require.NoError(t, err)
+
+	// adding a media-type to response is not breaking
+	require.Empty(t, d)
+}
