b2aad54919d201a3cb21d19aaa4ba19b36bf50ef
Reuven Harrison <rh@tufin.com>
2022-07-07 15:18:08 +0300

    Breaking changes doc links (#119)
    * add links to md
    
    * line-breaks
    
    * update script

--- doc_breaking_changes.sh
+++ doc_breaking_changes.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+echo "# Examples of Breaking and Non-Breaking Changes"
+echo "These examples are automatically generated from unit tests."
+
 echo "## Examples of breaking changes"
-grep -h '// BC:' diff/*.go | sed 's/^.*\/\/ BC: /- /g' | grep -v "isn't" | sort
+grep -n '// BC:' diff/*.go | grep -v "isn't" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
 
 echo ""
 echo "## Examples of non-breaking changes"
-grep -h '// BC:' diff/*.go | sed 's/^.*\/\/ BC: /- /g' | grep "isn't" | sort
+grep -n '// BC:' diff/*.go | grep "isn't" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
