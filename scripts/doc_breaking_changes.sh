9132fc6fe8d79fad57219c06bb015b67159d77aa
Reuven <rh@tufin.com>
2022-12-09 13:00:14 +0200

    regenerate breaking-changes.md from checker tests

--- scripts/doc_breaking_changes.sh
+++ scripts/doc_breaking_changes.sh
@@ -4,8 +4,8 @@ echo "# Examples of Breaking and Non-Breaking Changes"
 echo "These examples are automatically generated from unit tests."
 
 echo "## Examples of breaking changes"
-grep -n '// BC:' diff/*.go | grep "is breaking" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
+grep -n '// BC:' checker/*.go | grep "is breaking" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
 
 echo ""
 echo "## Examples of non-breaking changes"
-grep -n '// BC:' diff/*.go | grep "is not breaking" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
+grep -n '// BC:' checker/*.go | grep "is not breaking" | sed -n "s/\([^0-9]*\):\([0-9]*\):\/\/ BC: \(.*\)/[\3](\1?plain=1#L\2)  /p" | sort
