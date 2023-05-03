d16beeade6c4726d195bd626fe81c8af4e43f755
Reuven <rh@tufin.com>
2023-05-03 20:29:57 +0300

    regex cache

--- lint/check-regexp.go
+++ lint/check-regexp.go
@@ -7,6 +7,27 @@ import (
 	"github.com/tufin/oasdiff/load"
 )
 
+type regexCtx struct {
+	source string
+	cache  map[string]error
+}
+
+func newRegexCtx(source string) *regexCtx {
+	return &regexCtx{
+		source: source,
+		cache:  map[string]error{},
+	}
+}
+
+func (context *regexCtx) test(pattern string) error {
+	if result, ok := context.cache[pattern]; ok {
+		return result
+	}
+	_, err := regexp.Compile(pattern)
+	context.cache[pattern] = err
+	return err
+}
+
 // *** THIS IS A TEMPORARY IMPLEMENTATION ***
 // SHOULD USE ECMA 262, SEE: https://swagger.io/docs/specification/data-models/data-types/#pattern
 
@@ -18,109 +39,111 @@ func RegexCheck(source string, s *load.OpenAPISpecInfo) []*Error {
 		return result
 	}
 
+	context := newRegexCtx(source)
+
 	for _, path := range s.Spec.Paths {
-		result = append(result, checkParameters(path.Parameters, source)...)
-		result = append(result, checkOperations(path.Operations(), source)...)
+		result = append(result, checkParameters(path.Parameters, context)...)
+		result = append(result, checkOperations(path.Operations(), context)...)
 	}
 
 	return result
 }
 
-func checkOperations(operations map[string]*openapi3.Operation, source string) []*Error {
+func checkOperations(operations map[string]*openapi3.Operation, context *regexCtx) []*Error {
 	result := make([]*Error, 0)
 	for _, op := range operations {
 
-		result = append(result, checkParameters(op.Parameters, source)...)
+		result = append(result, checkParameters(op.Parameters, context)...)
 
 		if op.RequestBody != nil {
 			for _, mediaType := range op.RequestBody.Value.Content {
-				result = append(result, checkSchemaRef(mediaType.Schema, source)...)
+				result = append(result, checkSchemaRef(mediaType.Schema, context)...)
 			}
 		}
 
 		for _, response := range op.Responses {
 			for _, mediaType := range response.Value.Content {
-				result = append(result, checkSchemaRef(mediaType.Schema, source)...)
+				result = append(result, checkSchemaRef(mediaType.Schema, context)...)
 			}
 			for _, header := range response.Value.Headers {
-				result = append(result, checkSchemaRef(header.Value.Schema, source)...)
+				result = append(result, checkSchemaRef(header.Value.Schema, context)...)
 			}
 		}
 
 		for _, callback := range op.Callbacks {
 			for _, pathItem := range *callback.Value {
-				result = append(result, checkParameters(pathItem.Parameters, source)...)
-				result = append(result, checkOperations(pathItem.Operations(), source)...)
+				result = append(result, checkParameters(pathItem.Parameters, context)...)
+				result = append(result, checkOperations(pathItem.Operations(), context)...)
 			}
 		}
 	}
 	return result
 }
 
-func checkParameters(parameters openapi3.Parameters, source string) []*Error {
+func checkParameters(parameters openapi3.Parameters, context *regexCtx) []*Error {
 	result := make([]*Error, 0)
 	for _, parameter := range parameters {
 		if parameter.Value == nil {
 			continue
 		}
 		if parameter.Value.Schema != nil {
-			result = append(result, checkSchemaRef(parameter.Value.Schema, source)...)
+			result = append(result, checkSchemaRef(parameter.Value.Schema, context)...)
 		}
 		for _, mediaType := range parameter.Value.Content {
 			if mediaType.Schema != nil {
-				result = append(result, checkSchemaRef(mediaType.Schema, source)...)
+				result = append(result, checkSchemaRef(mediaType.Schema, context)...)
 			}
 		}
 	}
 	return result
 }
 
-func checkSchema(schema *openapi3.Schema, source string) []*Error {
+func checkSchema(schema *openapi3.Schema, context *regexCtx) []*Error {
 	result := make([]*Error, 0)
 
-	if err := checkRegex(schema.Pattern, source); err != nil {
+	if err := checkRegex(schema.Pattern, context); err != nil {
 		result = append(result, err)
 	}
 
 	for _, subSchema := range schema.OneOf {
-		result = append(result, checkSchemaRef(subSchema, source)...)
+		result = append(result, checkSchemaRef(subSchema, context)...)
 	}
 	for _, subSchema := range schema.AnyOf {
-		result = append(result, checkSchemaRef(subSchema, source)...)
+		result = append(result, checkSchemaRef(subSchema, context)...)
 	}
 	for _, subSchema := range schema.AllOf {
-		result = append(result, checkSchemaRef(subSchema, source)...)
+		result = append(result, checkSchemaRef(subSchema, context)...)
 	}
 	if schema.Not != nil {
-		result = append(result, checkSchemaRef(schema.Not, source)...)
+		result = append(result, checkSchemaRef(schema.Not, context)...)
 	}
 	if schema.Items != nil {
-		result = append(result, checkSchemaRef(schema.Items, source)...)
+		result = append(result, checkSchemaRef(schema.Items, context)...)
 	}
 	for _, subSchema := range schema.Properties {
-		result = append(result, checkSchemaRef(subSchema, source)...)
+		result = append(result, checkSchemaRef(subSchema, context)...)
 	}
 	if schema.AdditionalProperties.Schema != nil {
-		result = append(result, checkSchemaRef(schema.AdditionalProperties.Schema, source)...)
+		result = append(result, checkSchemaRef(schema.AdditionalProperties.Schema, context)...)
 	}
 	return result
 }
 
-func checkSchemaRef(schema *openapi3.SchemaRef, source string) []*Error {
-	return checkSchema(schema.Value, source)
+func checkSchemaRef(schema *openapi3.SchemaRef, context *regexCtx) []*Error {
+	return checkSchema(schema.Value, context)
 }
 
-func checkRegex(pattern string, source string) *Error {
+func checkRegex(pattern string, context *regexCtx) *Error {
 	if pattern == "" {
 		return nil
 	}
 
-	if _, err := regexp.Compile(pattern); err != nil {
+	if err := context.test(pattern); err != nil {
 		return &Error{
 			Id:     "invalid-regex-pattern",
 			Level:  LEVEL_ERROR,
 			Text:   err.Error(),
-			Source: source,
+			Source: context.source,
 		}
 	}
 
