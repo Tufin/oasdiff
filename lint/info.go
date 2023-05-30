78b0359116945215b30189b5619ec3dc7bc5391a
Reuven <rh@tufin.com>
2023-05-30 16:15:13 +0300

    updated info tests

--- lint/info.go
+++ lint/info.go
@@ -1,6 +1,9 @@
 package lint
 
 import (
+	"fmt"
+	"net/url"
+
 	"github.com/tufin/oasdiff/load"
 )
 
@@ -12,37 +15,43 @@ func InfoCheck(source string, spec *load.OpenAPISpecInfo) []*Error {
 		return result
 	}
 
+	if spec.Spec.Info == nil {
+		result = append(result, &Error{
+			Id:      "info-missing",
+			Level:   LEVEL_ERROR,
+			Text:    "Info is missing",
+			Comment: "It is a good practice to include general information about your API into the specification. Title and Version fields are required.",
+			Source:  source,
+		})
+		return result
+	}
+
 	if spec.Spec.Info.Title == "" {
 		result = append(result, &Error{
 			Id:     "info-title-missing",
-			Level:  LEVEL_WARN,
-			Text:   "It is a good practice to include general information about your API into the specification: title is missing",
+			Level:  LEVEL_ERROR,
+			Text:   "The title of the API is missing",
 			Source: source,
 		})
 	}
 	if spec.Spec.Info.Version == "" {
 		result = append(result, &Error{
 			Id:     "info-version-missing",
-			Level:  LEVEL_WARN,
-			Text:   "It is a good practice to include general information about your API into the specification: version number is missing",
+			Level:  LEVEL_ERROR,
+			Text:   "The version of the API is missing",
 			Source: source,
 		})
 	}
-	if spec.Spec.Info.License.Name == "" {
-		result = append(result, &Error{
-			Id:     "info-version-missing",
-			Level:  LEVEL_WARN,
-			Text:   "It is a good practice to include general information about your API into the specification: license name is missing",
-			Source: source,
-		})
-	}
-	if spec.Spec.Info.License.URL == "" {
-		result = append(result, &Error{
-			Id:     "info-version-missing",
-			Level:  LEVEL_WARN,
-			Text:   "It is a good practice to include general information about your API into the specification: license URL is missing",
-			Source: source,
-		})
+
+	if tos := spec.Spec.Info.TermsOfService; tos != "" {
+		if _, err := url.ParseRequestURI(tos); err != nil {
+			result = append(result, &Error{
+				Id:     "info-invalud-terms-of-service",
+				Level:  LEVEL_ERROR,
+				Text:   fmt.Sprintf("Terms of service must be in the format of a URL: %s", tos),
+				Source: source,
+			})
+		}
 	}
 
 	return result
