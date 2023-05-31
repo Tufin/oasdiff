dc09caac114f66cb46ff1875fae4e7d4ba074f00
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-05-31 14:24:35 +0100

    Update check-response-property-type-changed.go

--- checker/check-response-property-type-changed.go
+++ checker/check-response-property-type-changed.go
@@ -1,6 +1,7 @@
 package checker
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/tufin/oasdiff/diff"
@@ -49,6 +50,10 @@ func ResponsePropertyTypeChangedCheck(diffReport *diff.Diff, operationsSources *
 					CheckModifiedPropertiesDiff(
 						mediaTypeDiff.SchemaDiff,
 						func(propertyPath string, propertyName string, propertyDiff *diff.SchemaDiff, parent *diff.SchemaDiff) {
+							if propertyDiff == nil || propertyDiff.Revision == nil || propertyDiff.Revision.Value == nil {
+								return
+							}
+
 							if propertyDiff.Revision.Value.ReadOnly {
 								return
 							}
