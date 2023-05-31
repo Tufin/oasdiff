2fed103aa3095b40dc36f82170ff62c906dd981d
wtrocki <w.trocki@mongodb.com>
2023-05-31 11:49:21 +0200

    issue267: basic check for removal of schemas

---
+++ checker/check-components-schemas-removed.go
@@ -0,0 +1,28 @@
+package checker
+
+import (
+	"github.com/tufin/oasdiff/diff"
+)
+
+const (
+	apiSchemasRemovedCheckId = "api-model-removed"
+)
+
+func APIOperationSchemasRemovedCheck(diffReport *diff.Diff, operationsSources *diff.OperationsSourcesMap, config BackwardCompatibilityCheckConfig) []BackwardCompatibilityError {
+	result := make([]BackwardCompatibilityError, 0)
+	if diffReport.ComponentsDiff.SchemasDiff == nil {
+		return result
+	}
+
+	for _, pathItem := range diffReport.ComponentsDiff.SchemasDiff.Deleted {
+		result = append(result, BackwardCompatibilityError{
+			Id:        apiSchemasRemovedCheckId,
+			Level:     ERR,
+			Text:      config.i18n(apiSchemasRemovedCheckId),
+			Operation: "",
+			Path:      pathItem,
+			Source:    "",
+		})
+	}
+	return result
+}
