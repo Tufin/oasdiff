db4cd2e9cd79f38a36a073a2ee16cb308af53ba4
blva <40155621+blva@users.noreply.github.com>
2023-06-21 11:34:44 +0100

    Add more cl checks (#297)

---
+++ checker/check-request-body-required-value-updated_test.go
@@ -0,0 +1,59 @@
+package checker_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/checker"
+	"github.com/tufin/oasdiff/diff"
+)
+
+// BC: changing request's body to required is breaking
+func TestRequestBodyBecameRequired(t *testing.T) {
+	s1, _ := open("../data/checker/request_body_became_required_base.yaml")
+	s2, err := open("../data/checker/request_body_became_required_base.yaml")
+	require.Empty(t, err)
+
+	s2.Spec.Paths["/api/v1.0/groups"].Post.RequestBody.Value.Required = true
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibility(singleCheckConfig(checker.RequestBodyRequiredUpdatedCheck), d, osm)
+	require.NotEmpty(t, errs)
+	require.Equal(t, checker.BackwardCompatibilityErrors{
+		{
+			Id:          "request-body-became-required",
+			Text:        "request body became required",
+			Comment:     "",
+			Level:       checker.ERR,
+			Operation:   "POST",
+			Path:        "/api/v1.0/groups",
+			Source:      "../data/checker/request_body_became_required_base.yaml",
+			OperationId: "createOneGroup",
+		}}, errs)
+}
+
+// CL: changing request's body to optional
+func TestRequestBodyBecameOptional(t *testing.T) {
+	s1, _ := open("../data/checker/request_body_became_optional_base.yaml")
+	s2, err := open("../data/checker/request_body_became_optional_base.yaml")
+	require.Empty(t, err)
+
+	s2.Spec.Paths["/api/v1.0/groups"].Post.RequestBody.Value.Required = false
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig().WithCheckBreaking(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestBodyRequiredUpdatedCheck), d, osm, checker.INFO)
+	require.NotEmpty(t, errs)
+	require.Equal(t, checker.BackwardCompatibilityErrors{
+		{
+			Id:          "request-body-became-optional",
+			Text:        "request body became optional",
+			Comment:     "",
+			Level:       checker.INFO,
+			Operation:   "POST",
+			Path:        "/api/v1.0/groups",
+			Source:      "../data/checker/request_body_became_optional_base.yaml",
+			OperationId: "createOneGroup",
+		}}, errs)
+}
--- checker/default_checks.go
+++ checker/default_checks.go
@@ -39,9 +39,9 @@ func getBackwardCompatibilityCheckConfig(checks []BackwardCompatibilityCheck, le
 }
 
 var optionalChecks = map[string]BackwardCompatibilityCheck{
-	"response-non-success-status-removed":   ResponseNonSuccessStatusRemoved,
-	"api-operation-id-removed":              APIOperationIdRemovedCheck,
-	"api-tag-removed":                       APITagRemovedCheck,
+	"response-non-success-status-removed":   ResponseNonSuccessStatusUpdated,
+	"api-operation-id-removed":              APIOperationIdUpdatedCheck,
+	"api-tag-removed":                       APITagUpdatedCheck,
 	"api-schema-removed":                    APIComponentsSchemaRemovedCheck,
 	"response-property-enum-value-removed":  ResponseParameterEnumValueRemovedCheck,
 	"response-mediatype-enum-value-removed": ResponseMediaTypeEnumValueRemovedCheck,
@@ -75,11 +75,11 @@ func defaultChecks() []BackwardCompatibilityCheck {
 		ResponsePropertyBecameOptionalCheck,
 		ResponsePropertyBecameNullableCheck,
 		RequestPropertyBecameNotNullableCheck,
-		RequestBodyBecameRequiredCheck,
+		RequestBodyRequiredUpdatedCheck,
 		RequestBodyBecameEnumCheck,
 		ResponseHeaderBecameOptional,
 		ResponseHeaderRemoved,
-		ResponseSuccessStatusRemoved,
+		ResponseSuccessStatusUpdated,
 		ResponseMediaTypeRemoved,
 		NewRequestPathParameterCheck,
 		NewRequestNonPathParameterCheck,
