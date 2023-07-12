5a16595511178e2cd50689e9a3f71667e59621dd
Bianca <bianca.vianadeaguiar@mongodb.com>
2023-07-12 16:52:06 +0100

    Add request body media type update checker

---
+++ checker/check-request-body-mediatype-updated.go
@@ -0,0 +1,60 @@
+package checker
+
+import (
+	"fmt"
+
+	"github.com/tufin/oasdiff/diff"
+)
+
+const (
+	RequestBodyMediaTypeAdded   = "request-body-media-type-added"
+	RequestBodyMediaTypeRemoved = "request-body-media-type-removed"
+)
+
+func RequestBodyMediaTypeChangedCheck(diffReport *diff.Diff, operationsSources *diff.OperationsSourcesMap, config Config) Changes {
+	result := make(Changes, 0)
+	if diffReport.PathsDiff == nil {
+		return result
+	}
+	for path, pathItem := range diffReport.PathsDiff.Modified {
+		if pathItem.OperationsDiff == nil {
+			continue
+		}
+		for operation, operationItem := range pathItem.OperationsDiff.Modified {
+			if operationItem.RequestBodyDiff == nil ||
+				operationItem.RequestBodyDiff.ContentDiff == nil ||
+				operationItem.RequestBodyDiff.ContentDiff.MediaTypeModified == nil {
+				continue
+			}
+			source := (*operationsSources)[operationItem.Revision]
+
+			addedMediaTypes := operationItem.RequestBodyDiff.ContentDiff.MediaTypeAdded
+			for _, mediaType := range addedMediaTypes {
+				result = append(result, ApiChange{
+					Id:          RequestBodyMediaTypeAdded,
+					Level:       INFO,
+					Text:        fmt.Sprintf(config.i18n(RequestBodyMediaTypeAdded), mediaType),
+					Operation:   operation,
+					OperationId: operationItem.Revision.OperationID,
+					Path:        path,
+					Source:      source,
+				})
+			}
+
+			removedMediaTypes := operationItem.RequestBodyDiff.ContentDiff.MediaTypeDeleted
+			for _, mediaType := range removedMediaTypes {
+				result = append(result, ApiChange{
+					Id:          RequestBodyMediaTypeRemoved,
+					Level:       ERR,
+					Text:        fmt.Sprintf(config.i18n(RequestBodyMediaTypeRemoved), mediaType),
+					Operation:   operation,
+					OperationId: operationItem.Revision.OperationID,
+					Path:        path,
+					Source:      source,
+				})
+			}
+
+		}
+	}
+	return result
+}
