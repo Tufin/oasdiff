6fa0e004c63e12706da17b08b641e0a5375182c7
blva <40155621+blva@users.noreply.github.com>
2023-07-14 10:24:24 +0100

    Add response property read only write only checkers (#322)

---
+++ checker/check-response-optional-property-write-only-read-only_test.go
@@ -0,0 +1,113 @@
+package checker_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/checker"
+	"github.com/tufin/oasdiff/diff"
+)
+
+// CL: Changing optional response property to write-only
+func TestResponseOptionalPropertyBecameWriteOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["name"].Value.WriteOnly = true
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseOptionalPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-optional-property-became-write-only",
+		Text:        "the response optional property 'data/name' became write-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_optional_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing optional response property to not write-only
+func TestResponseOptionalPropertyBecameNotWriteOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["writeOnlyName"].Value.WriteOnly = false
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseOptionalPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-optional-property-became-not-write-only",
+		Text:        "the response optional property 'data/writeOnlyName' became not write-only for the status '200'",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_optional_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing optional response property to read-only
+func TestResponseOptionalPropertyBecameReadOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s1.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["id"].Value.ReadOnly = false
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseOptionalPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-optional-property-became-read-only",
+		Text:        "the response optional property 'data/id' became read-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_optional_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing optional response property to not read-only
+func TestResponseOptionalPropertyBecameNonReadOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_optional_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["id"].Value.ReadOnly = false
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseOptionalPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-optional-property-became-not-read-only",
+		Text:        "the response optional property 'data/id' became not read-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_optional_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
---
+++ checker/check-response-required-property-write-only-read-only_test.go
@@ -0,0 +1,114 @@
+package checker_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/checker"
+	"github.com/tufin/oasdiff/diff"
+)
+
+// CL: Changing required response property to write-only
+func TestResponseRequiredPropertyBecameWriteOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["name"].Value.WriteOnly = true
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseRequiredPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-required-property-became-write-only",
+		Text:        "the response required property 'data/name' became write-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_required_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing required response property to not write-only
+func TestResponseRequiredPropertyBecameNotWriteOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["writeOnlyName"].Value.WriteOnly = false
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseRequiredPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-required-property-became-not-write-only",
+		Text:        "the response required property 'data/writeOnlyName' became not write-only for the status '200'",
+		Comment:     "It is valid only if the property was always returned before the specification has been changed",
+		Level:       checker.WARN,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_required_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing required response property to read-only
+func TestResponseRequiredPropertyBecameReadOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s1.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["id"].Value.ReadOnly = false
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseRequiredPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-required-property-became-read-only",
+		Text:        "the response required property 'data/id' became read-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_required_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing required response property to not read-only
+func TestResponseRequiredPropertyBecameNonReadOnly(t *testing.T) {
+	s1, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2, err := open("../data/checker/response_required_property_write_only_read_only_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Components.Schemas["GroupView"].Value.Properties["data"].Value.Properties["id"].Value.ReadOnly = false
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.ResponseRequiredPropertyWriteOnlyReadOnlyCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+
+		Id:          "response-required-property-became-not-read-only",
+		Text:        "the response required property 'data/id' became not read-only for the status '200'",
+		Comment:     "",
+		Level:       checker.INFO,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/response_required_property_write_only_read_only_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
