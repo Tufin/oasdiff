c907ac0591742ef41322fe20ec265a29030cb4be
Bianca <bianca.vianadeaguiar@mongodb.com>
2023-07-24 13:56:19 +0100

    request param change: type

---
+++ checker/check-request-parameter-became-enum_test.go
@@ -0,0 +1,32 @@
+package checker_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tufin/oasdiff/checker"
+	"github.com/tufin/oasdiff/diff"
+)
+
+// CL: Changing request parameter type to enum
+func TestRequestParameterBecameEnum(t *testing.T) {
+	s1, err := open("../data/checker/request_parameter_became_enum_base.yaml")
+	require.NoError(t, err)
+	s2, err := open("../data/checker/request_parameter_became_enum_revision.yaml")
+	require.NoError(t, err)
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterBecameEnumCheck), d, osm, checker.ERR)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+		Id:          "request-parameter-became-enum",
+		Text:        "the 'path' request parameter 'groupId' was restricted to a list of enum values",
+		Comment:     "",
+		Level:       checker.ERR,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/request_parameter_became_enum_revision.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
