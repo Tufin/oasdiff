dce2c5d423a7e3057cbdf48c9a4444e6af0ef581
blva <40155621+blva@users.noreply.github.com>
2023-07-25 08:32:48 +0100

    Changelog: request parameters changed (#336)

--- checker/check-request-parameters-type-changed_test.go
+++ checker/check-request-parameters-type-changed_test.go
@@ -82,3 +82,78 @@ func TestRequestQueryHeaderTypeChanged(t *testing.T) {
 		OperationId: "createOneGroup",
 	}, errs[0])
 }
+
+// CL: Changing request path parameter format
+func TestRequestPathParamFormatChanged(t *testing.T) {
+	s1, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+	s2, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Paths["/api/v1.0/groups"].Post.Parameters[0].Value.Schema.Value.Format = "uuid"
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterTypeChangedCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+		Id:          "request-parameter-type-changed",
+		Text:        "for the 'path' request parameter 'groupId', the type/format was changed from 'string'/'none' to 'string'/'uuid'",
+		Comment:     "",
+		Level:       checker.ERR,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/request_parameter_type_changed_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing request query parameter format
+func TestRequestQueryParamFormatChanged(t *testing.T) {
+	s1, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+	s2, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Paths["/api/v1.0/groups"].Post.Parameters[1].Value.Schema.Value.Format = "uri"
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterTypeChangedCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+		Id:          "request-parameter-type-changed",
+		Text:        "for the 'query' request parameter 'token', the type/format was changed from 'string'/'uuid' to 'string'/'uri'",
+		Comment:     "",
+		Level:       checker.ERR,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/request_parameter_type_changed_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// CL: Changing request header parameter format
+func TestRequestQueryHeaderFormatChanged(t *testing.T) {
+	s1, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+	s2, err := open("../data/checker/request_parameter_type_changed_base.yaml")
+	require.NoError(t, err)
+
+	s2.Spec.Paths["/api/v1.0/groups"].Post.Parameters[2].Value.Schema.Value.Format = "uri"
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(getConfig(), s1, s2)
+	require.NoError(t, err)
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterTypeChangedCheck), d, osm, checker.INFO)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+		Id:          "request-parameter-type-changed",
+		Text:        "for the 'header' request parameter 'X-Request-ID', the type/format was changed from 'string'/'uuid' to 'string'/'uri'",
+		Comment:     "",
+		Level:       checker.ERR,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      "../data/checker/request_parameter_type_changed_base.yaml",
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
