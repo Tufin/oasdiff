bf70927289b0590685410fbb3b0975a7ab7c3094
Ciprian Tibulca <ciprian.tibulca@mongodb.com>
2023-07-26 16:24:52 +0100

    implement request/response anyOf checkers (#339)

---
+++ checker/check-request-property-any-of-updated.go
@@ -0,0 +1,103 @@
+package checker
+
+import (
+	"fmt"
+
+	"github.com/tufin/oasdiff/diff"
+)
+
+func RequestPropertyAnyOfUpdated(diffReport *diff.Diff, operationsSources *diff.OperationsSourcesMap, config Config) Changes {
+	result := make(Changes, 0)
+	if diffReport.PathsDiff == nil {
+		return result
+	}
+
+	for path, pathItem := range diffReport.PathsDiff.Modified {
+		if pathItem.OperationsDiff == nil {
+			continue
+		}
+
+		for operation, operationItem := range pathItem.OperationsDiff.Modified {
+			if operationItem.RequestBodyDiff == nil ||
+				operationItem.RequestBodyDiff.ContentDiff == nil ||
+				operationItem.RequestBodyDiff.ContentDiff.MediaTypeModified == nil {
+				continue
+			}
+			source := (*operationsSources)[operationItem.Revision]
+
+			modifiedMediaTypes := operationItem.RequestBodyDiff.ContentDiff.MediaTypeModified
+			for _, mediaTypeDiff := range modifiedMediaTypes {
+				if mediaTypeDiff.SchemaDiff == nil {
+					continue
+				}
+
+				if mediaTypeDiff.SchemaDiff.AnyOfDiff != nil && len(mediaTypeDiff.SchemaDiff.AnyOfDiff.Added) > 0 {
+					result = append(result, ApiChange{
+						Id:    "request-body-any-of-added",
+						Level: INFO,
+						Text: fmt.Sprintf(
+							config.i18n("request-body-any-of-added"),
+							ColorizedValue(mediaTypeDiff.SchemaDiff.AnyOfDiff.Added.String())),
+						Operation:   operation,
+						OperationId: operationItem.Revision.OperationID,
+						Path:        path,
+						Source:      source,
+					})
+				}
+
+				if mediaTypeDiff.SchemaDiff.AnyOfDiff != nil && len(mediaTypeDiff.SchemaDiff.AnyOfDiff.Deleted) > 0 {
+					result = append(result, ApiChange{
+						Id:    "request-body-any-of-removed",
+						Level: ERR,
+						Text: fmt.Sprintf(
+							config.i18n("request-body-any-of-removed"),
+							ColorizedValue(mediaTypeDiff.SchemaDiff.AnyOfDiff.Deleted.String())),
+						Operation:   operation,
+						OperationId: operationItem.Revision.OperationID,
+						Path:        path,
+						Source:      source,
+					})
+				}
+
+				CheckModifiedPropertiesDiff(
+					mediaTypeDiff.SchemaDiff,
+					func(propertyPath string, propertyName string, propertyDiff *diff.SchemaDiff, parent *diff.SchemaDiff) {
+						if propertyDiff.AnyOfDiff == nil {
+							return
+						}
+
+						if len(propertyDiff.AnyOfDiff.Added) > 0 {
+							result = append(result, ApiChange{
+								Id:    "request-property-any-of-added",
+								Level: INFO,
+								Text: fmt.Sprintf(
+									config.i18n("request-property-any-of-added"),
+									ColorizedValue(propertyDiff.AnyOfDiff.Added.String()),
+									ColorizedValue(propertyFullName(propertyPath, propertyName))),
+								Operation:   operation,
+								OperationId: operationItem.Revision.OperationID,
+								Path:        path,
+								Source:      source,
+							})
+						}
+
+						if len(propertyDiff.AnyOfDiff.Deleted) > 0 {
+							result = append(result, ApiChange{
+								Id:    "request-property-any-of-removed",
+								Level: ERR,
+								Text: fmt.Sprintf(
+									config.i18n("request-property-any-of-removed"),
+									ColorizedValue(propertyDiff.AnyOfDiff.Deleted.String()),
+									ColorizedValue(propertyFullName(propertyPath, propertyName))),
+								Operation:   operation,
+								OperationId: operationItem.Revision.OperationID,
+								Path:        path,
+								Source:      source,
+							})
+						}
+					})
+			}
+		}
+	}
+	return result
+}
---
+++ checker/check-response-property-any-of-updated.go
@@ -0,0 +1,111 @@
+package checker
+
+import (
+	"fmt"
+
+	"github.com/tufin/oasdiff/diff"
+)
+
+func ResponsePropertyAnyOfUpdated(diffReport *diff.Diff, operationsSources *diff.OperationsSourcesMap, config Config) Changes {
+	result := make(Changes, 0)
+	if diffReport.PathsDiff == nil {
+		return result
+	}
+
+	for path, pathItem := range diffReport.PathsDiff.Modified {
+		if pathItem.OperationsDiff == nil {
+			continue
+		}
+
+		for operation, operationItem := range pathItem.OperationsDiff.Modified {
+			if operationItem.ResponsesDiff == nil || operationItem.ResponsesDiff.Modified == nil {
+				continue
+			}
+			source := (*operationsSources)[operationItem.Revision]
+
+			for responseStatus, responsesDiff := range operationItem.ResponsesDiff.Modified {
+				if responsesDiff.ContentDiff == nil || responsesDiff.ContentDiff.MediaTypeModified == nil {
+					continue
+				}
+
+				modifiedMediaTypes := responsesDiff.ContentDiff.MediaTypeModified
+				for _, mediaTypeDiff := range modifiedMediaTypes {
+					if mediaTypeDiff.SchemaDiff == nil {
+						continue
+					}
+
+					if mediaTypeDiff.SchemaDiff.AnyOfDiff != nil && len(mediaTypeDiff.SchemaDiff.AnyOfDiff.Added) > 0 {
+						result = append(result, ApiChange{
+							Id:    "response-body-any-of-added",
+							Level: INFO,
+							Text: fmt.Sprintf(
+								config.i18n("response-body-any-of-added"),
+								ColorizedValue(mediaTypeDiff.SchemaDiff.AnyOfDiff.Added.String()),
+								responseStatus),
+							Operation:   operation,
+							OperationId: operationItem.Revision.OperationID,
+							Path:        path,
+							Source:      source,
+						})
+					}
+
+					if mediaTypeDiff.SchemaDiff.AnyOfDiff != nil && len(mediaTypeDiff.SchemaDiff.AnyOfDiff.Deleted) > 0 {
+						result = append(result, ApiChange{
+							Id:    "response-body-any-of-removed",
+							Level: INFO,
+							Text: fmt.Sprintf(
+								config.i18n("response-body-any-of-removed"),
+								ColorizedValue(mediaTypeDiff.SchemaDiff.AnyOfDiff.Deleted.String()),
+								responseStatus),
+							Operation:   operation,
+							OperationId: operationItem.Revision.OperationID,
+							Path:        path,
+							Source:      source,
+						})
+					}
+
+					CheckModifiedPropertiesDiff(
+						mediaTypeDiff.SchemaDiff,
+						func(propertyPath string, propertyName string, propertyDiff *diff.SchemaDiff, parent *diff.SchemaDiff) {
+							if propertyDiff.AnyOfDiff == nil {
+								return
+							}
+
+							if len(propertyDiff.AnyOfDiff.Added) > 0 {
+								result = append(result, ApiChange{
+									Id:    "response-property-any-of-added",
+									Level: INFO,
+									Text: fmt.Sprintf(
+										config.i18n("response-property-any-of-added"),
+										ColorizedValue(propertyDiff.AnyOfDiff.Added.String()),
+										ColorizedValue(propertyFullName(propertyPath, propertyName)),
+										responseStatus),
+									Operation:   operation,
+									OperationId: operationItem.Revision.OperationID,
+									Path:        path,
+									Source:      source,
+								})
+							}
+
+							if len(propertyDiff.AnyOfDiff.Deleted) > 0 {
+								result = append(result, ApiChange{
+									Id:    "response-property-any-of-removed",
+									Level: INFO,
+									Text: fmt.Sprintf(
+										config.i18n("response-property-any-of-removed"),
+										ColorizedValue(propertyDiff.AnyOfDiff.Deleted.String()),
+										ColorizedValue(propertyFullName(propertyPath, propertyName)),
+										responseStatus),
+									Operation:   operation,
+									OperationId: operationItem.Revision.OperationID,
+									Path:        path,
+									Source:      source,
+								})
+							}
+						})
+				}
+			}
+		}
+	}
+	return result
+}
