50b694122b86d4ab3aaf5b1d5e316c5a09d30e8f
Reuven <rh@tufin.com>
2023-09-19 14:45:02 +0300

    use NoFileCompletions with NoArgs

--- internal/checks.go
+++ internal/checks.go
@@ -10,10 +10,11 @@ import (
 func getChecksCmd() *cobra.Command {
 
 	cmd := cobra.Command{
-		Use:   "checks [flags]",
-		Short: "Display optional checks",
-		Long:  `Display optional checks`,
-		Args:  cobra.ExactArgs(0),
+		Use:               "checks [flags]",
+		Short:             "Display optional checks",
+		Long:              `Display optional checks that can be passed to 'breaking' and 'changelog' with the 'include-checks' flag`,
+		Args:              cobra.NoArgs,
+		ValidArgsFunction: cobra.NoFileCompletions, // see https://github.com/spf13/cobra/issues/1969
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			// by now flags have been parsed successfully so we don't need to show usage on any errors
