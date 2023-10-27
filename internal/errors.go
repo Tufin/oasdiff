497b1f7d45a4dbbd881547d05423f174b7254fd3
Reuven <rh@tufin.com>
2023-10-27 09:09:24 +0300

    fix golangci issue

--- internal/errors.go
+++ internal/errors.go
@@ -46,13 +46,6 @@ func getErrFailedPrint(what string, err error) *ReturnError {
 	}
 }
 
-func getErrUnsupportedFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("unsupported format %q", format),
-		Code:  108,
-	}
-}
-
 func getErrUnsupportedDiffFormat(format string) *ReturnError {
 	return &ReturnError{
 		error: fmt.Errorf("format %q is not supported by \"diff\"", format),
