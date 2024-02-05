e4d777b29e0d688286bfbe970fccdd5450139bac
Reuven Harrison <rh@tufin.com>
2024-02-05 10:46:54 +0200

    refactor errors (#483)

--- internal/errors.go
+++ internal/errors.go
@@ -11,86 +11,92 @@ type ReturnError struct {
 	Code int
 }
 
+const generalExecutionErr = 100
+
 func getErrInvalidFlags(err error) *ReturnError {
-	return &ReturnError{
-		error: err,
-		Code:  101,
-	}
+	return getError(
+		err,
+		101,
+	)
 }
 
 func getErrFailedToLoadSpec(what string, source *load.Source, err error) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("failed to load %s spec from %s with %v", what, source.Out(), err),
-		Code:  102,
-	}
+	return getError(
+		fmt.Errorf("failed to load %s spec from %s with %v", what, source.Out(), err),
+		102,
+	)
 }
 
 func getErrFailedToLoadSpecs(what string, path string, err error) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("failed to load %s specs from glob %q with %v", what, path, err),
-		Code:  103,
-	}
+	return getError(
+		fmt.Errorf("failed to load %s specs from glob %q with %v", what, path, err),
+		103,
+	)
 }
 
 func getErrDiffFailed(err error) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("diff failed with %v", err),
-		Code:  104,
-	}
+	return getError(
+		fmt.Errorf("diff failed with %v", err),
+		104,
+	)
 }
 
 func getErrFailedPrint(what string, err error) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("failed to print %q with %v", what, err),
-		Code:  105,
-	}
+	return getError(
+		fmt.Errorf("failed to print %q with %v", what, err),
+		105,
+	)
 }
 
 func getErrUnsupportedDiffFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("format %q is not supported by \"diff\"", format),
-		Code:  109,
-	}
+	return getError(
+		fmt.Errorf("format %q is not supported by \"diff\"", format),
+		109,
+	)
 }
 
 func getErrUnsupportedSummaryFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("format %q is not supported by \"summary\"", format),
-		Code:  110,
-	}
+	return getError(
+		fmt.Errorf("format %q is not supported by \"summary\"", format),
+		110,
+	)
 }
 
 func getErrUnsupportedChangelogFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("format %q is not supported by \"changelog\"", format),
-		Code:  111,
-	}
+	return getError(
+		fmt.Errorf("format %q is not supported by \"changelog\"", format),
+		111,
+	)
 }
 
 func getErrUnsupportedBreakingChangesFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("format %q is not supported by \"breaking\"", format),
-		Code:  112,
-	}
+	return getError(
+		fmt.Errorf("format %q is not supported by \"breaking\"", format),
+		112,
+	)
 }
 
 func getErrUnsupportedChecksFormat(format string) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("format %q is not supported with \"checks\"", format),
-		Code:  113,
-	}
+	return getError(
+		fmt.Errorf("format %q is not supported with \"checks\"", format),
+		113,
+	)
 }
 
 func getErrInvalidColorMode(err error) *ReturnError {
-	return &ReturnError{
-		error: err,
-		Code:  114,
-	}
+	return getError(
+		err,
+		114,
+	)
 }
 
 func getErrCantProcessIgnoreFile(what string, err error) *ReturnError {
-	return &ReturnError{
-		error: fmt.Errorf("can't process %s ignore file %v", what, err),
-		Code:  121,
-	}
+	return getError(
+		fmt.Errorf("can't process %s ignore file %v", what, err),
+		121,
+	)
+}
+
+func getError(err error, code int) *ReturnError {
+	return &ReturnError{err, code}
 }
