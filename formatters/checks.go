2d9834dae7582152be1d8518686a8abf45b3d982
Raphael Huefner <raphaelhuefner@users.noreply.github.com>
2024-02-13 08:49:11 +0100

    YAML/JSON field name typo: "reuired" -> "required" (#491)

--- formatters/checks.go
+++ formatters/checks.go
@@ -4,7 +4,7 @@ type Check struct {
 	Id          string `json:"id" yaml:"id"`
 	Level       string `json:"level" yaml:"level"`
 	Description string `json:"description" yaml:"description"`
-	Required    bool   `json:"reuired" yaml:"reuired"`
+	Required    bool   `json:"required" yaml:"required"`
 }
 
 type Checks []Check
