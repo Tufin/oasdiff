534c199763efc7ced811faf3dced1c3af6790a12
Reuven <rh@tufin.com>
2024-04-15 16:35:09 +0300

    improve unit tests

--- diff/diff_x_of_titles_test.go
+++ diff/diff_x_of_titles_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/require"
 	"github.com/tufin/oasdiff/diff"
+	"github.com/tufin/oasdiff/utils"
 )
 
 func getXOfTitlesFile(file string) string {
@@ -40,8 +41,8 @@ func TestXOfTitles_TitleNameChanged(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, dd)
 	anyOfDiff := dd.PathsDiff.Modified["/test"].OperationsDiff.Modified["GET"].ResponsesDiff.Modified["200"].ContentDiff.MediaTypeModified["application/json"].SchemaDiff.AnyOfDiff
-	require.Len(t, anyOfDiff.Added, 1)
-	require.Len(t, anyOfDiff.Deleted, 1)
+	require.ElementsMatch(t, utils.StringList{"RevisionSchema[1]:Title 3"}, anyOfDiff.Added)
+	require.ElementsMatch(t, utils.StringList{"BaseSchema[1]:Title 2"}, anyOfDiff.Deleted)
 	require.Empty(t, anyOfDiff.Modified)
 }
 
@@ -61,6 +62,8 @@ func TestXOfTitles_TitlesModified(t *testing.T) {
 	require.Empty(t, anyOfDiff.Added)
 	require.Empty(t, anyOfDiff.Deleted)
 	require.Len(t, anyOfDiff.Modified, 2)
+	require.Equal(t, diff.ValueDiff{From: "string", To: "boolean"}, *anyOfDiff.Modified["Title 1"].TypeDiff)
+	require.Equal(t, diff.ValueDiff{From: "string", To: "number"}, *anyOfDiff.Modified["Title 2"].TypeDiff)
 }
 
 func TestXOfTitles_TitlesModifiedAndAdded(t *testing.T) {
@@ -76,7 +79,26 @@ func TestXOfTitles_TitlesModifiedAndAdded(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, dd)
 	anyOfDiff := dd.PathsDiff.Modified["/test"].OperationsDiff.Modified["GET"].ResponsesDiff.Modified["200"].ContentDiff.MediaTypeModified["application/json"].SchemaDiff.AnyOfDiff
-	require.Len(t, anyOfDiff.Added, 1)
+	require.ElementsMatch(t, utils.StringList{"RevisionSchema[2]:Title 3"}, anyOfDiff.Added)
 	require.Empty(t, anyOfDiff.Deleted)
 	require.Len(t, anyOfDiff.Modified, 1)
+	require.Equal(t, diff.ValueDiff{From: "boolean", To: "string"}, *anyOfDiff.Modified["Title 1"].TypeDiff)
+}
+
+func TestXOfTitles_DuplicateTitles(t *testing.T) {
+	loader := openapi3.NewLoader()
+
+	s1, err := loader.LoadFromFile(getXOfTitlesFile("spec3.yaml"))
+	require.NoError(t, err)
+
+	s2, err := loader.LoadFromFile(getXOfTitlesFile("spec5.yaml"))
+	require.NoError(t, err)
+
+	dd, err := diff.Get(&diff.Config{}, s1, s2)
+	require.NoError(t, err)
+	require.NotEmpty(t, dd)
+	anyOfDiff := dd.PathsDiff.Modified["/test"].OperationsDiff.Modified["GET"].ResponsesDiff.Modified["200"].ContentDiff.MediaTypeModified["application/json"].SchemaDiff.AnyOfDiff
+	require.ElementsMatch(t, utils.StringList{"RevisionSchema[0]:Title 2", "RevisionSchema[2]:Title 3"}, anyOfDiff.Added)
+	require.ElementsMatch(t, utils.StringList{"BaseSchema[0]:Title 1"}, anyOfDiff.Deleted)
+	require.Empty(t, anyOfDiff.Modified, 0)
 }
