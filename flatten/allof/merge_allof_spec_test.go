aa8fceae7eccbd7d2506edade618fdf3b705006c
Omer E <33223663+tcdsv@users.noreply.github.com>
2024-04-24 12:49:27 +0300

    flatten-allof fix circular additionalProperties overflow (#529)

--- flatten/allof/merge_allof_spec_test.go
+++ flatten/allof/merge_allof_spec_test.go
@@ -26,3 +26,48 @@ func Test_MergeSpecInvalid(t *testing.T) {
 	_, err := load.NewSpecInfo(openapi3.NewLoader(), load.NewSource("../../data/allof/invalid.yaml"), load.WithFlattenAllOf())
 	require.EqualError(t, err, "failed to flatten allOf in \"../../data/allof/invalid.yaml\": unable to resolve Type conflict: all Type values must be identical")
 }
+
+func TestMergeSpec_CircularAdditionalPropsWithoutAllOf(t *testing.T) {
+	spec, err := load.NewSpecInfo(openapi3.NewLoader(), load.NewSource("testdata/circular_additional_props1.yaml"), load.WithFlattenAllOf())
+	require.NoError(t, err)
+
+	merged := spec.Spec
+	require.Equal(t, "object", merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.Type)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value)
+
+	baseSchema := merged.Components.Schemas["BaseSchema"].Value
+	referencedAdditionalPropSchema := merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value
+	require.Equal(t, baseSchema, referencedAdditionalPropSchema)
+}
+
+func TestMergeSpec_MergeCircularAdditionalPropsWithAllOf(t *testing.T) {
+	spec, err := load.NewSpecInfo(openapi3.NewLoader(), load.NewSource("testdata/circular_additional_props2.yaml"), load.WithFlattenAllOf())
+	require.NoError(t, err)
+
+	merged := spec.Spec
+	require.Nil(t, merged.Components.Schemas["BaseSchema"].Value.AllOf)
+	require.Equal(t, "string", merged.Components.Schemas["BaseSchema"].Value.Properties["fixedProperty"].Value.Type)
+	require.Equal(t, "object", merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.Type)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value)
+
+	baseSchema := merged.Components.Schemas["BaseSchema"].Value
+	referencedAdditionalPropSchema := merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value
+	require.Equal(t, baseSchema, referencedAdditionalPropSchema)
+}
+
+func TestMergeSpec_MergeCircularAdditionalPropsNestedWithinAllOf(t *testing.T) {
+	spec, err := load.NewSpecInfo(openapi3.NewLoader(), load.NewSource("testdata/circular_additional_props3.yaml"), load.WithFlattenAllOf())
+	require.NoError(t, err)
+
+	merged := spec.Spec
+	require.Nil(t, merged.Components.Schemas["BaseSchema"].Value.AllOf)
+	require.Equal(t, "object", merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.Type)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema)
+	require.NotNil(t, merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value)
+
+	baseSchemaReferencedAdditionalPropSchema := merged.Components.Schemas["BaseSchema"].Value.Properties["prop1"].Value.AdditionalProperties.Schema.Value
+	NestedSelfReferentialSchema := merged.Components.Schemas["NestedSelfReferentialSchema"].Value
+	require.Equal(t, baseSchemaReferencedAdditionalPropSchema, NestedSelfReferentialSchema)
+}
