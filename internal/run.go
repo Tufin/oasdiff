061cae7e4061b6390466b28a9f25c5302d378154
Reuven <rh@tufin.com>
2024-05-04 11:42:29 +0300

    remove telemetry

--- internal/run.go
+++ internal/run.go
@@ -1,30 +1,18 @@
 package internal
 
 import (
-	"context"
 	"io"
-	"os"
 	"strconv"
-	"time"
 
-	"github.com/oasdiff/go-common/util"
-	"github.com/oasdiff/telemetry/client"
-	"github.com/oasdiff/telemetry/model"
 	"github.com/spf13/cobra"
 	"github.com/tufin/oasdiff/build"
 )
 
 func Run(args []string, stdout io.Writer, stderr io.Writer) int {
 
-	ctx, cancelCtx := context.WithDeadline(context.Background(), time.Now().Add(model.DefaultTimeout))
-	defer cancelCtx()
-
-	chanPreRun := make(chan int)
 	rootCmd := &cobra.Command{
-		Use:               "oasdiff",
-		Short:             "OpenAPI specification diff",
-		PersistentPreRun:  func(cmd *cobra.Command, args []string) { preRun(chanPreRun, cmd) },
-		PersistentPostRun: func(*cobra.Command, []string) { postRun(ctx, chanPreRun) },
+		Use:   "oasdiff",
+		Short: "OpenAPI specification diff",
 	}
 
 	rootCmd.SetArgs(args[1:])
@@ -43,33 +31,6 @@ func Run(args []string, stdout io.Writer, stderr io.Writer) int {
 	)
 
 	return run(rootCmd)
-}
-
-func preRun(c chan int, cmd *cobra.Command) {
-
-	go func(c chan int) {
-		defer close(c)
-		if os.Getenv(model.EnvNoTelemetry) != "1" {
-			_ = client.NewCollector(util.NewStringSet().Add("err-ignore").
-				Add("warn-ignore").
-				Add("match-path").
-				Add("prefix-base").
-				Add("prefix-revision").
-				Add("strip-prefix-base").
-				Add("strip-prefix-revision").
-				Add("filter-extension")).SendCommand(cmd)
-		}
-	}(c)
-}
-
-func postRun(ctx context.Context, c chan int) {
-
-	select {
-	case <-c:
-		break
-	case <-ctx.Done():
-		break
-	}
 }
 
 func setReturnValue(cmd *cobra.Command, code int) {
