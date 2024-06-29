b0adf26e83dd8b428288a14ce869c10e460968af
Reuven Harrison <reuvenharrison@gmail.com>
2024-06-29 22:03:18 +0300

    improve code and comments (#571)
    Co-authored-by: Reuven <rh@tufin.com>

--- checker/check_request_parameters_type_changed.go
+++ checker/check_request_parameters_type_changed.go
@@ -40,7 +40,7 @@ func RequestParameterTypeChangedCheck(diffReport *diff.Diff, operationsSources *
 
 						result = append(result, ApiChange{
 							Id:          RequestParameterTypeChangedId,
-							Level:       conditionalError(breakingTypeFormatChangedInRequestParam(typeDiff, formatDiff, schemaDiff), INFO),
+							Level:       conditionalError(breakingTypeFormatChangedInRequest(typeDiff, formatDiff, false, schemaDiff), INFO),
 							Args:        []any{paramLocation, paramName, getBaseType(schemaDiff), getBaseFormat(schemaDiff), getRevisionType(schemaDiff), getRevisionFormat(schemaDiff)},
 							Operation:   operation,
 							OperationId: operationItem.Revision.OperationID,
@@ -78,38 +78,4 @@ func RequestParameterTypeChangedCheck(diffReport *diff.Diff, operationsSources *
 		}
 	}
 	return result
-}
-
-/*
-checkRequestParameterPropertyTypeChanged checks the level of the change in the request parameter property type
-Explanation:
-Objects can be passed in the request parameters, for example, the following calls are equivalent:
-PHP style: GET http://localhost:8080/api/tickets?params[id]=123&params[color]=green
-JSON: GET http://localhost:8080/api/tickets?params={"id":"123","color":"green"}
-
-The "params" object has two properties: "id" and "color", both with type "string", but note that the "id" values are actually numbers.
-Imagine that the OpenAPI type of property "id" was changed from "number" to "string".
-In the first example, the change is non-breaking, because the PHP format for numbers and strings is the same.
-But in the second example, the change is breaking, because the JSON format requires quotes for strings.
-*/
-func checkRequestParameterPropertyTypeChanged(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, schemaDiff *diff.SchemaDiff) (Level, string) {
-
-	// try with JSON format
-	isBreakingAsJson := breakingTypeFormatChangedInRequestProperty(typeDiff, formatDiff, "application/json", schemaDiff)
-
-	// try with non-JSON format
-	isBreakingAsNonJson := breakingTypeFormatChangedInRequestProperty(typeDiff, formatDiff, "", schemaDiff)
-
-	// if the JSON and not breaking as non-JSON formats don't agree, it's a warning
-	if isBreakingAsJson != isBreakingAsNonJson {
-		return WARN, RequestParameterPropertyTypeChangedCommentId
-	}
-
-	// if both are breaking it's an error
-	if isBreakingAsJson {
-		return ERR, ""
-	}
-
-	// if niether are breaking it's an informational change
-	return INFO, ""
 }
--- checker/check_types.go
+++ checker/check_types.go
@@ -8,23 +8,36 @@ import (
 	"github.com/tufin/oasdiff/utils"
 )
 
+// breakingTypeFormatChangedInResponseProperty checks if the type or format of a response property was changed in a breaking way
 func breakingTypeFormatChangedInResponseProperty(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, mediaType string, schemaDiff *diff.SchemaDiff) bool {
 
 	if typeDiff != nil {
-		return !isTypeContained(typeDiff.Deleted, typeDiff.Added, isStronglyTyped(mediaType))
+		typeDiff = &diff.StringsDiff{
+			Added:   typeDiff.Deleted,
+			Deleted: typeDiff.Added,
+		}
 	}
 
 	if formatDiff != nil {
-		return !isFormatContained(schemaDiff.Revision.Type, formatDiff.From, formatDiff.To)
+		formatDiff = &diff.ValueDiff{
+			From: formatDiff.To,
+			To:   formatDiff.From,
+		}
 	}
 
-	return false
+	return breakingTypeFormatChangedInRequestProperty(typeDiff, formatDiff, mediaType, schemaDiff)
 }
 
+// breakingTypeFormatChangedInRequestProperty checks if the type or format of a request property was changed in a breaking way
 func breakingTypeFormatChangedInRequestProperty(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, mediaType string, schemaDiff *diff.SchemaDiff) bool {
+	return breakingTypeFormatChangedInRequest(typeDiff, formatDiff, isStronglyTyped(mediaType), schemaDiff)
+}
+
+// breakingTypeFormatChangedInRequest checks if the type or format of a request was changed in a breaking way
+func breakingTypeFormatChangedInRequest(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, stronglyTyped bool, schemaDiff *diff.SchemaDiff) bool {
 
 	if typeDiff != nil {
-		return !isTypeContained(typeDiff.Added, typeDiff.Deleted, isStronglyTyped(mediaType))
+		return !isTypeContained(typeDiff.Added, typeDiff.Deleted, stronglyTyped)
 	}
 
 	if formatDiff != nil {
@@ -34,21 +47,10 @@ func breakingTypeFormatChangedInRequestProperty(typeDiff *diff.StringsDiff, form
 	return false
 }
 
-func breakingTypeFormatChangedInRequestParam(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, schemaDiff *diff.SchemaDiff) bool {
-
-	if typeDiff != nil {
-		return !isTypeContained(typeDiff.Added, typeDiff.Deleted, false)
-	}
-
-	if formatDiff != nil {
-		return !isFormatContained(schemaDiff.Revision.Type, formatDiff.To, formatDiff.From)
-	}
-
-	return false
-}
-
-// isTypeContained checks if type2 is contained in type1
-// note that we don't support multiple types currenty
+/*
+isTypeContained checks if type2 is contained in type1
+note that we don't support multiple types currenty
+*/
 func isTypeContained(to, from utils.StringList, stronglyTyped bool) bool {
 
 	if to.Is("number") && from.Is("integer") {
@@ -63,9 +65,42 @@ func isTypeContained(to, from utils.StringList, stronglyTyped bool) bool {
 	return false
 }
 
-// isStronglyTyped checks if the media type is strongly typed, for example:
-// in text format, all numbers can also be interpreted as strings (1 can be a number or a string)
-// but in json, a number (1) is not the same as a string ("1")
+/*
+checkRequestParameterPropertyTypeChanged checks the level of the change in the request parameter property type
+Explanation:
+Objects can be passed in the request parameters, for example, the following calls are equivalent:
+PHP style: GET http://localhost:8080/api/tickets?params[id]=123&params[color]=green
+JSON: GET http://localhost:8080/api/tickets?params={"id":"123","color":"green"}
+
+The "params" object has two properties: "id" and "color", both with type "string", but note that the "id" values are actually numbers.
+Imagine that the OpenAPI type of property "id" was changed from "number" to "string".
+In the first example, the change is non-breaking, because the PHP format for numbers and strings is the same: we refer to this as non-strongly-typed.
+But in the second example, the change is breaking, because the JSON format requires quotes for strings: we refer to this as strongly-typed.
+*/
+func checkRequestParameterPropertyTypeChanged(typeDiff *diff.StringsDiff, formatDiff *diff.ValueDiff, schemaDiff *diff.SchemaDiff) (Level, string) {
+
+	stronglyTyped := breakingTypeFormatChangedInRequest(typeDiff, formatDiff, true, schemaDiff)
+	nonStronglyTyped := breakingTypeFormatChangedInRequest(typeDiff, formatDiff, false, schemaDiff)
+
+	// if strongly-typed and non-strongly-typed don't agree, it's a warning since we can't be sure that it's breaking
+	if stronglyTyped != nonStronglyTyped {
+		return WARN, RequestParameterPropertyTypeChangedCommentId
+	}
+
+	// if both are breaking it's an error
+	if stronglyTyped {
+		return ERR, ""
+	}
+
+	// if niether are breaking it's an informational change
+	return INFO, ""
+}
+
+/*
+isStronglyTyped checks if the media type is strongly typed, for example:
+in text format, all numbers can also be interpreted as strings (1 can be a number or a string)
+but in json, a number (1) is not the same as a string ("1")
+*/
 func isStronglyTyped(mediaType string) bool {
 	return isJsonMediaType(mediaType)
 }
