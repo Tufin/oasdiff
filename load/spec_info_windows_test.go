fbbeb8f8dec651e149ce98dd5f25b2b17c7a7f22
Reuven <rh@tufin.com>
2024-07-09 15:22:27 +0300

    update windows err messsages

--- load/spec_info_windows_test.go
+++ load/spec_info_windows_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestLoadInfo_FileWindows(t *testing.T) {
 	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource(`C:\dev\OpenApi\spec2.yaml`))
-	require.EqualError(t, err, "open C:/dev/OpenApi/spec2.yaml: The system cannot find the path specified.")
+	require.EqualError(t, err, "open C:\\dev\\OpenApi\\spec2.yaml: The system cannot find the path specified.")
 }
 
 func TestLoadInfo_UriInvalid(t *testing.T) {
 	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource("http://localhost/null"))
-	require.EqualError(t, err, "open ../null: The system cannot find the file specified.")
+	require.EqualError(t, err, "open ..\\null: The system cannot find the file specified.")
 }
 
 func TestLoadInfo_UriBadScheme(t *testing.T) {
 	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource("ftp://localhost/null"))
-	require.EqualError(t, err, "open ftp://localhost/null: The filename, directory name, or volume label syntax is incorrect.")
+	require.EqualError(t, err, "open ftp:\\\\localhost\\null: The filename, directory name, or volume label syntax is incorrect.")
 }
