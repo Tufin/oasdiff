3c6483d00bc236e8fc205a9bcd8583a2b209264b
Reuven <rh@tufin.com>
2024-07-09 15:17:59 +0300

    separate unix and win tests

--- load/spec_info_test.go
+++ load/spec_info_test.go
@@ -13,33 +13,9 @@ func TestSpecInfo_File(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestLoadInfo_FileWindows(t *testing.T) {
-	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource(`C:\dev\OpenApi\spec2.yaml`))
-	require.Condition(t, func() (success bool) {
-		return err.Error() == "open C:\\dev\\OpenApi\\spec2.yaml: no such file or directory" ||
-			err.Error() == "open C:/dev/OpenApi/spec2.yaml: The system cannot find the path specified."
-	})
-}
-
 func TestLoadInfo_URI(t *testing.T) {
 	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource("https://localhost/data/openapi-test1.yaml"))
 	require.NoError(t, err)
-}
-
-func TestLoadInfo_UriInvalid(t *testing.T) {
-	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource("http://localhost/null"))
-	require.Condition(t, func() (success bool) {
-		return err.Error() == "open ../null: no such file or directory" ||
-			err.Error() == "open ../null: The system cannot find the file specified."
-	})
-}
-
-func TestLoadInfo_UriBadScheme(t *testing.T) {
-	_, err := load.NewSpecInfo(MockLoader{}, load.NewSource("ftp://localhost/null"))
-	require.Condition(t, func() (success bool) {
-		return err.Error() == "open ftp://localhost/null: no such file or directory" ||
-			err.Error() == "open ftp://localhost/null: The filename, directory name, or volume label syntax is incorrect."
-	})
 }
 
 func TestLoadInfo_Stdin(t *testing.T) {
