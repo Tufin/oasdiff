05bb6a226f328f7e42975005613d688d71b928c6
Reuven Harrison <reuvenharrison@gmail.com>
2024-08-29 22:20:20 +0300

    add script to print untested ids

--- scripts/get_tested_ids.sh
+++ scripts/get_tested_ids.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $1 == "-v" ]]; then
+if [[ $1 == "lines" ]]; then
     grep -nRE --exclude-dir=localizations,localizations_src "// (CL|BC): .*: .*-.*" checker/* | cut -d":" -f1,2,5
-else
+elif [[ $1 == "ids" ]]; then
     grep -nRE --exclude-dir=localizations,localizations_src '// (CL|BC): .*: .*-.*' checker/* | cut -d: -f5 | tr , '\n' | awk '{$1=$1};1' | sort -u
+elif [[ $1 == "diff" ]]; then
+    diff --side-by-side <(grep -nRE --exclude-dir=localizations,localizations_src '// (CL|BC): .*: .*-.*' checker/* | cut -d: -f5 | tr , '\n' | awk '{$1=$1};1' | sort -u) <(cat checker/localizations_src/en/messages.yaml | sed '/# warnings/q' | cut -d":" -f1 | sort -n)
 fi
