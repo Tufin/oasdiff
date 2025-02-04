bc26e163008a32c3cffd7aecd095ca6f7a6b72f4
Reuven Harrison <reuvenharrison@gmail.com>
2025-02-04 14:36:35 +0200

    test breaking-changes for common-params

--- checker/check_request_parameters_max_items_updated_test.go
+++ checker/check_request_parameters_max_items_updated_test.go
@@ -3,6 +3,7 @@ package checker_test
 import (
 	"testing"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/require"
 	"github.com/tufin/oasdiff/checker"
 	"github.com/tufin/oasdiff/diff"
@@ -49,6 +50,47 @@ func TestRequestParameterMaxItemsDecreased(t *testing.T) {
 		Operation:   "POST",
 		Path:        "/api/v1.0/groups",
 		Source:      load.NewSource("../data/checker/request_parameter_max_items_updated_base.yaml"),
+		OperationId: "createOneGroup",
+	}, errs[0])
+}
+
+// BC: decreasing maxItems of common request parameters without --flatten-params is not breaking
+func TestBreaking_RequestParameterMaxItemsWithoutFlatten(t *testing.T) {
+
+	s1, err := open("../data/common-params/request_parameter_max_items_updated_revision.yaml")
+	require.NoError(t, err)
+	s2, err := open("../data/common-params/request_parameter_max_items_updated_base.yaml")
+	require.NoError(t, err)
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(diff.NewConfig(), s1, s2)
+	require.NoError(t, err)
+
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterMaxItemsUpdatedCheck), d, osm, checker.ERR)
+	require.Empty(t, errs)
+}
+
+// BC: decreasing maxItems of common request parameters with --flatten-params is breaking
+func TestBreaking_RequestParameterMaxItemsWithFlatten(t *testing.T) {
+	loader := openapi3.NewLoader()
+
+	s1, err := load.NewSpecInfo(loader, load.NewSource("../data/common-params/request_parameter_max_items_updated_revision.yaml"), load.WithFlattenParams())
+	require.NoError(t, err)
+
+	s2, err := load.NewSpecInfo(loader, load.NewSource("../data/common-params/request_parameter_max_items_updated_base.yaml"), load.WithFlattenParams())
+	require.NoError(t, err)
+
+	d, osm, err := diff.GetWithOperationsSourcesMap(diff.NewConfig(), s1, s2)
+	require.NoError(t, err)
+
+	errs := checker.CheckBackwardCompatibilityUntilLevel(singleCheckConfig(checker.RequestParameterMaxItemsUpdatedCheck), d, osm, checker.ERR)
+	require.Len(t, errs, 1)
+	require.Equal(t, checker.ApiChange{
+		Id:          checker.RequestParameterMaxItemsDecreasedId,
+		Args:        []any{"query", "category", uint64(20), uint64(10)},
+		Level:       checker.ERR,
+		Operation:   "POST",
+		Path:        "/api/v1.0/groups",
+		Source:      load.NewSource("../data/common-params/request_parameter_max_items_updated_base.yaml"),
 		OperationId: "createOneGroup",
 	}, errs[0])
 }
